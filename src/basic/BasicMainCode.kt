bf367449d4eee5bf1501ab919b86336d54895e43
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-16 22:02:36 +0900

    [2025-02-16]

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -1,23 +1,12 @@
 package basic
 
-import kotlin.math.roundToInt
-
-
 class BasicMainCode {
-    /**
-     * @param numbers 정수 배열
-     * @return 원소 평균값
-     */
-    fun solution(numbers: IntArray): Double {
-        val avg = numbers.average()
-//        return (avg * 2).roundToInt() / 2.0
-//        return ((avg * 2).toInt() + 0.5).toInt() / 2.0
-        val decimal = avg - avg.toInt()
 
-        return when {
-            decimal < 0.25 -> avg.toInt().toDouble()
-            decimal < 0.75 -> avg.toInt() + 0.5
-            else -> avg.toInt() + 1.0
-        }
+    companion object{
+        fun solution(n: Int, k: Int): Int = (n * 12000) + ((k - (n / 10)) * 2000)
     }
+}
+
+fun main(){
+    println(BasicMainCode.solution(64, 6))
 }
