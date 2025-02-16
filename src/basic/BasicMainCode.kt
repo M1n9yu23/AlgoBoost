7c48c0ac5291cb98ed4b1e65093f6bbf90ad1665
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-16 22:19:43 +0900

    [2025-02-16]

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -1,12 +1,25 @@
 package basic
 
+import kotlin.math.sqrt
+
 class BasicMainCode {
 
     companion object{
-        fun solution(n: Int, k: Int): Int = (n * 12000) + ((k - (n / 10)) * 2000)
+        fun solution(n: Int): Int {
+            var sum = 0
+            for(i in 1..sqrt(n.toDouble()).toInt()){
+                if(n % i == 0){
+                    sum += i
+                    if (i != n / i) {
+                        sum += n / i
+                    }
+                }
+            }
+            return sum
+        }
     }
 }
 
 fun main(){
-    println(BasicMainCode.solution(64, 6))
+    println(BasicMainCode.solution(5))
 }
