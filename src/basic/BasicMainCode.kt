9801bac8a91d84cdbe1d0042e237f27daaa9e5bd
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-21 19:35:25 +0900

    [2025-02-21]: 배열 뒤집기

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -4,30 +4,11 @@ class BasicMainCode {
 
     // 짝수의 합
     companion object{
-        fun solution(str1: String, str2: String): Int {
-            val len1 = str1.length
-            val len2 = str2.length
-
-            for (i in 0..(len1 - len2)) {
-                var bool = true
-
-                for(j in 0 until len2){
-                    if(str1[i + j] != str2[j]){
-                        bool = false
-                        break
-                    }
-                }
-
-                if(bool) {
-                    return 1
-                }
-            }
-
-            return 2
-        }
+        fun solution(num_list: IntArray): IntArray =
+            num_list.mapIndexed { index, _ -> num_list[num_list.size - 1 - index] }.toIntArray()
     }
 }
 
 fun main(){
-    println(BasicMainCode.solution("ab6CDE443fgh22iJKlmn1o", "6CD"))
+    println(BasicMainCode.solution(intArrayOf(1,2,3,4,5)))
 }
