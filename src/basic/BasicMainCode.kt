5bfeb96578a6fb747a04e9397aebcb7e215e1404
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-21 19:06:27 +0900

    [2025-02-21]: 문자열안에 문자열 API 사용하지 않고 기본 문법으로만

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -4,12 +4,30 @@ class BasicMainCode {
 
     // 짝수의 합
     companion object{
-        fun solution(n: Int): Int = (0..n).filter{
-            it % 2 == 0
-        }.sum()
+        fun solution(str1: String, str2: String): Int {
+            val len1 = str1.length
+            val len2 = str2.length
+
+            for (i in 0..(len1 - len2)) {
+                var bool = true
+
+                for(j in 0 until len2){
+                    if(str1[i + j] != str2[j]){
+                        bool = false
+                        break
+                    }
+                }
+
+                if(bool) {
+                    return 1
+                }
+            }
+
+            return 2
+        }
     }
 }
 
 fun main(){
-    println(BasicMainCode.solution(10))
+    println(BasicMainCode.solution("ab6CDE443fgh22iJKlmn1o", "6CD"))
 }
