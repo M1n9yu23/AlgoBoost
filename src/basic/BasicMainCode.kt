4b19d82ed7cf5847f363db8d634a095272bc21ca
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-21 18:47:51 +0900

    [2025-02-21]: 짝수의 합

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -2,13 +2,14 @@ package basic
 
 class BasicMainCode {
 
+    // 짝수의 합
     companion object{
-        fun solution(n: Int): Int = n.toString().sumOf{
-            it - '0'
-        }
+        fun solution(n: Int): Int = (0..n).filter{
+            it % 2 == 0
+        }.sum()
     }
 }
 
 fun main(){
-    println(BasicMainCode.solution(123))
+    println(BasicMainCode.solution(10))
 }
