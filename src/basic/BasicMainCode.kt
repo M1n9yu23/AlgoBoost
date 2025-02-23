f492b060c5239285cbad9e768f25924f4568a165
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-23 13:34:19 +0900

    [2025-02-21]: 편지

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -2,12 +2,12 @@ package basic
 
 class BasicMainCode {
 
+    // 편지
     companion object{
-        fun solution(num_list: IntArray): IntArray =
-            num_list.mapIndexed { index, _ -> num_list[num_list.size - 1 - index] }.toIntArray()
+        fun solution(message: String): Int = message.length * 2
     }
 }
 
 fun main(){
-    println(BasicMainCode.solution(intArrayOf(1,2,3,4,5)))
+    println(BasicMainCode.solution("I love you~"))
 }
