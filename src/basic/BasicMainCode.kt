bf527b91dacff5b810a63d1efd548586e284b876
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-24 20:14:46 +0900

    [2025-02-23]: 더 크게 합치기

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,21 +3,14 @@ package basic
 class BasicMainCode {
 
     companion object{
-        fun solution(my_string: String, letter: String): String {
-            var result = ""
-            val letterChar = letter[0]
-
-            for(i in my_string.indices){
-                if(my_string[i] != letterChar) {
-                    result += my_string[i]
-                }
-            }
-
-            return result
+        fun solution(a: Int, b: Int): Int {
+            val x = "$a$b".toInt()
+            val y = "$b$a".toInt()
+            return maxOf(x, y)
         }
     }
 }
 
 fun main(){
-    println(BasicMainCode.solution("abcdef", "f"))
+    println(BasicMainCode.solution(12, 3))
 }
