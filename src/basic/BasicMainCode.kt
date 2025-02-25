42921ae47c769eebbc323c0d46f98ed2afdeaa27
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-25 21:21:53 +0900

    [2025-02-25]: 주사위 게임 2

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -1,24 +1,25 @@
 package basic
 
+import kotlin.math.pow
+
+
 class BasicMainCode {
 
-    companion object{
-        fun solution(a: Int, d: Int, included: BooleanArray): Int {
-            var sum = 0
-            var current = a
-
-            for(isIncluded in included) {
-                if(isIncluded) {
-                    sum += current
-                }
-                current += d
+    companion object {
+        fun solution(a: Int, b: Int, c: Int): Int {
+            val dice = listOf(a, b, c)
+            val x2 = dice.sumOf { it * it }
+            val x3 = dice.sumOf { it * it * it}
+            val sum = dice.sum()
+            return when (dice.toSet().size) {
+                1 -> sum * x2 * x3
+                2 -> sum * x2
+                else -> sum
             }
-
-            return sum
         }
     }
 }
 
-fun main(){
-    println(BasicMainCode.solution(3,4, booleanArrayOf(true, false, false, true, true)))
+fun main() {
+    println(BasicMainCode.solution(4, 4, 4))
 }
