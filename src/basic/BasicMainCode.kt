49310e1388a5d548a440541f99bfd3c5f781a9a3
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-26 19:33:38 +0900

    [2025-02-26]: 마지막 두 원소

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -4,10 +4,15 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(num_list: IntArray) = num_list.filter { it % 2 == 0 }.joinToString("").toInt() + num_list.filter { it % 2 != 0 }.joinToString("").toInt()
+        fun solution(num_list: IntArray): IntArray {
+            val last = num_list.last()
+            val lastPrev = num_list[num_list.size - 2]
+
+            return num_list + if(last > lastPrev) last - lastPrev else last * 2
+        }
     }
 }
 
 fun main() {
-    println(BasicMainCode.solution(intArrayOf(3,4,5,2,1)))
+    println(BasicMainCode.solution(intArrayOf(2,1,6)).toList())
 }
