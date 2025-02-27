dd847814a77c66c114935a305580f58c838c99f8
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-27 19:59:07 +0900

    [2025-02-27]: 배열 만들기 2

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,28 +3,21 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
+        fun solution(l: Int, r: Int): IntArray {
+            val answer = mutableListOf<Int>()
 
-            queries.forEach { (s, e, k) ->
-
-                for (i in s..e) {
-                    if (i % k == 0){
-                        arr[i] +=  1
-                    }
+            for (i in l..r) {
+                if(i.toString().all { it == '0' || it == '5'}) {
+                    answer.add(i)
                 }
             }
 
-            return arr
+            return if(answer.isEmpty()) intArrayOf(-1) else answer.toIntArray()
         }
     }
 }
 
 fun main() {
-    val arr = intArrayOf(0, 1, 2, 4, 3)
-    val queries = arrayOf(
-        intArrayOf(0, 4, 1),
-        intArrayOf(0, 3, 2),
-        intArrayOf(0, 3, 3)
-    )
-    println(BasicMainCode.solution(arr, queries).joinToString(", "))
+
+    println(BasicMainCode.solution(5, 555).joinToString(","))
 }
