b9c66d9a6f8d59d55ae3372122f83d5ac0bbe4d0
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-02-28 19:44:31 +0900

    [2025-02-28]: 카운트 업

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,21 +3,11 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(l: Int, r: Int): IntArray {
-            val answer = mutableListOf<Int>()
-
-            for (i in l..r) {
-                if(i.toString().all { it == '0' || it == '5'}) {
-                    answer.add(i)
-                }
-            }
-
-            return if(answer.isEmpty()) intArrayOf(-1) else answer.toIntArray()
-        }
+        fun solution(start_num: Int, end_num: Int) = (start_num..end_num).toList().toIntArray()
     }
 }
 
 fun main() {
 
-    println(BasicMainCode.solution(5, 555).joinToString(","))
+    println(BasicMainCode.solution(3,10).joinToString(","))
 }
