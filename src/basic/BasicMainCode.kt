dc410aad6b33b8fa20e628f102c764e9a4b84820
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-07 03:33:09 +0900

    [2025-03-05]: 9로 나눈 나머지

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -1,23 +1,12 @@
 package basic
 
-import kotlin.math.abs
-
 class BasicMainCode {
 
     companion object {
-        fun solution(my_string: String, index_list: IntArray): String {
-            var str = ""
-
-            for(i in index_list) {
-                str += my_string[i]
-            }
-
-            return str
-        }
+        fun solution(number: String) = number.sumOf { it - '0' } % 9
     }
 }
 
 fun main() {
-
-    println(BasicMainCode.solution("cvsgiorszzzmrpaqpe",intArrayOf(16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7)))
+    println(BasicMainCode.solution("78720646226947352489"))
 }
