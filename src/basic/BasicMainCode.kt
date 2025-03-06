dbfde222e89e839f17df61eb0bd2defdfc6143f8
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-03-07 03:14:56 +0900

    [2025-03-05]: 글자 이어 붙여 문자열 만들기

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -5,45 +5,19 @@ import kotlin.math.abs
 class BasicMainCode {
 
     companion object {
-        fun solution(a: Int, b: Int, c: Int, d: Int): Int {
-            val nums = listOf(a,b,c,d)
-            val count = mutableMapOf<Int, Int>()
+        fun solution(my_string: String, index_list: IntArray): String {
+            var str = ""
 
-            for(i in nums) {
-                count[i] = count.getOrDefault(i, 0) + 1
+            for(i in index_list) {
+                str += my_string[i]
             }
 
-            if(count.size == 1) {
-                return 1111 * a
-            }
-
-            for((k,v) in count) {
-                if(v == 3) {
-                    val oth = count.keys.first { it != k }
-                    return (10 * k + oth) * (10 * k + oth)
-                }
-            }
-
-            if(count.size == 2) {
-                val ks = count.keys.toList()
-                val vs = count.values.toList()
-                if(vs[0] == 2 && vs[1] == 2) {
-                    return (ks[0] + ks[1]) * abs(ks[0] - ks[1])
-                }
-            }
-
-            if(count.size == 3) {
-                val pr = count.entries.first { it.value == 2}.key
-                val oths = count.keys.filter { it != pr }
-                return oths[0] * oths[1]
-            }
-
-            return nums.minOrNull()!!
+            return str
         }
     }
 }
 
 fun main() {
 
-    println(BasicMainCode.solution(4,1,4,4))
+    println(BasicMainCode.solution("cvsgiorszzzmrpaqpe",intArrayOf(16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7)))
 }
