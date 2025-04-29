3f7cd54d25be831e8cd34955a06a33884e6e8517
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-04-29 16:24:05 +0900

    [2025-4-29]: 배열의 원소만큼 추가

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,17 +3,23 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(arr1: IntArray, arr2: IntArray): Int {
-            return if(arr1.size > arr2.size) 1
-            else if(arr1.size == arr2.size) {
-                if(arr1.sum() > arr2.sum()) 1
-                else if (arr1.sum() < arr2.sum()) -1
-                else 0
-            } else -1
+        fun solution(arr1: IntArray): IntArray {
+            val x = IntArray(arr1.sum())
+            var index = 0
+
+            for (i in arr1.indices) {
+                var c = arr1[i]
+                while(c > 0) {
+                    x[index++] = arr1[i]
+                    c--
+                }
+            }
+
+            return x
         }
     }
 }
 
 fun main() {
-    println(BasicMainCode.solution(intArrayOf(1, 2, 3, 4, 5), intArrayOf(3, 3, 3, 3, 3)))
+    println(BasicMainCode.solution(intArrayOf(5, 1, 4)).contentToString())
 }
