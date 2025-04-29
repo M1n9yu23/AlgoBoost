6b05d718014bf5b063b769631506610c536d7fe0
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-04-29 16:07:59 +0900

    [2025-4-29]: 배열 비교하기

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,10 +3,17 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(number: String) = number.sumOf { it - '0' } % 9
+        fun solution(arr1: IntArray, arr2: IntArray): Int {
+            return if(arr1.size > arr2.size) 1
+            else if(arr1.size == arr2.size) {
+                if(arr1.sum() > arr2.sum()) 1
+                else if (arr1.sum() < arr2.sum()) -1
+                else 0
+            } else -1
+        }
     }
 }
 
 fun main() {
-    println(BasicMainCode.solution("78720646226947352489"))
+    println(BasicMainCode.solution(intArrayOf(1, 2, 3, 4, 5), intArrayOf(3, 3, 3, 3, 3)))
 }
