e40a0886e4028a82bc8ae5611954903f75b86449
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-04-30 23:33:33 +0900

    [2025-4-29]: rny_string

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,23 +3,11 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(arr1: IntArray): IntArray {
-            val x = IntArray(arr1.sum())
-            var index = 0
+        fun solution(rny_string: String) = rny_string.replace("m", "rn")
 
-            for (i in arr1.indices) {
-                var c = arr1[i]
-                while(c > 0) {
-                    x[index++] = arr1[i]
-                    c--
-                }
-            }
-
-            return x
-        }
     }
 }
 
 fun main() {
-    println(BasicMainCode.solution(intArrayOf(5, 1, 4)).contentToString())
+    println(BasicMainCode.solution("masterpiece"))
 }
