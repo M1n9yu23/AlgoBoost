09043b2d1a0955c63ef213fe1270e4eaa1073120
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-05-01 23:22:02 +0900

    [2025-5-01]: x 사이의 개수

--- src/basic/BasicMainCode.kt
+++ src/basic/BasicMainCode.kt
@@ -3,10 +3,18 @@ package basic
 class BasicMainCode {
 
     companion object {
-        fun solution(my_string: String, n: Int) = my_string.substring(0 until n)
+        fun solution(myString: String): IntArray {
+            val st = myString.split("x")
+            val result = IntArray(st.size)
+            st.forEachIndexed { i, e ->
+                result[i] = e.length
+            }
+
+            return result
+        }
     }
 }
 
 fun main() {
-    println(BasicMainCode.solution("He110W0r1d", 5))
+    println(BasicMainCode.solution("oxooxoxxox").joinToString())
 }
